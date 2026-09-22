ea180ff0ca15e28be5130f1daf7ffac3a35e8b16
d4vtz <57779526+d4vtz@users.noreply.github.com>
2026-09-21 20:26:07 -0600

    feat: add config/hypr/modules/autostart.lua

---
+++ config/hypr/modules/autostart.lua
@@ -0,0 +1,7 @@
+hl.on("hyprland.start", function()
+  hl.exec_cmd("hyprpaper")
+  hl.exec_cmd("hypridle")
+  hl.exec_cmd("qs -c desktop")
+  hl.exec_cmd("wl-paste --type text --watch cliphist store")
+  hl.exec_cmd("wl-paste --type image --watch cliphist store")
+end)
