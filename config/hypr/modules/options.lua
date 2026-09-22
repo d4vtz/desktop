2f8ab89d8922accf3e4c1d0345b1a402d9695848
d4vtz <57779526+d4vtz@users.noreply.github.com>
2026-09-21 20:25:47 -0600

    feat: add config/hypr/modules/options.lua

---
+++ config/hypr/modules/options.lua
@@ -0,0 +1,17 @@
+hl.config({
+  general = {
+    layout = "dwindle",
+    gaps_in = 5,
+    gaps_out = 10,
+    border_size = 2,
+    resize_on_border = true,
+  },
+  misc = {
+    disable_hyprland_logo = true,
+    disable_splash_rendering = true,
+    vfr = true,
+  },
+  ecosystem = {
+    enforce_permissions = true,
+  },
+})
