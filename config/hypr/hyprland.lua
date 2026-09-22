2ca3e794287df81988b99f3040a93285e4929398
d4vtz <57779526+d4vtz@users.noreply.github.com>
2026-09-21 20:25:45 -0600

    feat: add config/hypr/hyprland.lua

---
+++ config/hypr/hyprland.lua
@@ -0,0 +1,10 @@
+-- d4vtz/desktop - Hyprland Lua entrypoint
+-- Hyprland >= 0.55
+require("modules.options")
+require("modules.monitors")
+require("modules.input")
+require("modules.appearance")
+require("modules.workspaces")
+require("modules.rules")
+require("modules.binds")
+require("modules.autostart")
