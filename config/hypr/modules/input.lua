c02b899fd4f40927b3cb0a4a1424a7cc1f367712
d4vtz <57779526+d4vtz@users.noreply.github.com>
2026-09-21 20:25:51 -0600

    feat: add config/hypr/modules/input.lua

---
+++ config/hypr/modules/input.lua
@@ -0,0 +1,16 @@
+hl.config({
+  input = {
+    kb_layout = "latam",
+    follow_mouse = 1,
+    sensitivity = 0,
+    touchpad = {
+      natural_scroll = true,
+      tap_to_click = true,
+      disable_while_typing = true,
+    },
+  },
+  gestures = {
+    workspace_swipe = true,
+    workspace_swipe_fingers = 3,
+  },
+})
